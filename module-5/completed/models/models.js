055b78a5487d9654c58845b5e0ae02a750853273
praveenmontoo <praveenmontoo@gmail.com>
2016-08-22 18:08:22 +0530

    first commit

---
+++ module-5/completed/models/models.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var postSchema = new mongoose.Schema({
+	created_by: String,		//should be changed to ObjectId, ref "User"
+	created_at: {type: Date, default: Date.now},
+	text: String
+});
+
+var userSchema = new mongoose.Schema({
+	username: String,
+	password: String, //hash created from password
+	emailId: String,
+	mobileNo:String,
+	created_at: {type: Date, default: Date.now}
+})
+
+
+mongoose.model('Post', postSchema);
+mongoose.model('User', userSchema);
